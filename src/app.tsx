4ee0c3848136f3d8c7872c1503769bf06ca7de01
pchchv <ipchchv@gmail.com>
2024-09-19 10:16:42 +0300

    Implement body style handling

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,20 @@
+import { useEffect } from 'react';
+import { useAppSelector } from './redux/hooks';
+
+
 function App() {
-    document.body.classList.add('bg-gray-50')
-    document.body.classList.remove('bg-darkbackground')
+    const darkMode = useAppSelector((state) => state.ui.darkMode)
+
+    // handle body styling
+    useEffect(() => {
+        if (darkMode) {
+            document.body.classList.remove('bg-gray-50')
+            document.body.classList.add('bg-darkbackground')
+        } else {
+            document.body.classList.add('bg-gray-50')
+            document.body.classList.remove('bg-darkbackground')
+        }
+    }, [darkMode])
 }
 
 export default App;
