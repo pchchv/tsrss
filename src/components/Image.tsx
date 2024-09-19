b131313dc69cf077ce772892f34c403e4579c7fd
pchchv <ipchchv@gmail.com>
2024-09-19 12:32:26 +0300

    Implement an Image

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,8 +1,31 @@
-import { CSSProperties } from 'react'
+import { CSSProperties, useState } from 'react'
 
 interface ImageProps {
-    className: string,
-    src: string | undefined,
-    FallbackComponent: () => JSX.Element
-    style?: CSSProperties
+  className: string,
+  src: string | undefined,
+  FallbackComponent: () => JSX.Element
+  style?: CSSProperties
+}
+
+export default function Image({ className, src, FallbackComponent, style }: ImageProps) { 
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(false)
+  // if src is undefined return fallback
+  if (!src) {
+    return <FallbackComponent/>
+  }
+  
+  return (
+    <>
+      { (loading || error) && <FallbackComponent/> }
+      <img
+        className={className + ( (loading || error) && " hidden")}
+        style={style}
+        src={src}
+        alt=""
+        onError={() => setError(true)}
+        onLoad={() => setLoading(false)}
+      />
+    </>
+  )
 }
