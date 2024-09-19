a87e771ef169c424e4cf08b8cb1b1bdf33468e68
pchchv <ipchchv@gmail.com>
2024-09-19 10:01:08 +0300

    Implement a store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,16 @@
-import { combineReducers } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import feedReducer from "./feedSlice";
 import uiReducer from "./uiSlice";
+import { loadState } from "./localStorage"
+
+const persistedState = loadState()
 
 const rootReducer = combineReducers({
     feed: feedReducer,
     ui: uiReducer
+})
+
+const store = configureStore({
+    reducer: rootReducer,
+    preloadedState: persistedState
 })
