149089e7d28c277448b41f311a2a5b2b8ecdf601
pchchv <ipchchv@gmail.com>
2024-09-19 09:59:11 +0300

    Implement INITIAL_STATE and uiSlice

--- src/redux/uiSlice.ts
+++ src/redux/uiSlice.ts
@@ -1,3 +1,5 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
 type SideContainerViewType = "none" | "create_feed" | "create_provider" | "manage_feed" | "delete_feed"
 
 // Pass whatever params inside.
@@ -10,3 +12,29 @@ interface InitialReduxState{
     darkMode: boolean,
     sideViewType: SideContainerViewProp
 }
+
+const INITIAL_STATE: InitialReduxState = {
+    darkMode: false,
+    sideViewType: { viewName: "none" }
+}
+
+const uiSlice = createSlice({
+    name:"uiSlice",
+    initialState: INITIAL_STATE,
+    reducers: {
+        toggleDarkMode(state){
+            state.darkMode = !state.darkMode
+        },
+
+        showView(state, action: PayloadAction<SideContainerViewProp>){
+            state.sideViewType = action.payload
+
+        }
+    }  
+})
+
+
+export const { toggleDarkMode, showView } = uiSlice.actions
+
+
+export default uiSlice.reducer
