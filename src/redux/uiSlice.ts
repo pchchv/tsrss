50c29d16e902d3fa44c4259dff30fe4f6f7ea161
pchchv <ipchchv@gmail.com>
2024-09-19 09:57:47 +0300

    Implement SideContainerViewType and SideContainerViewProp

---
+++ src/redux/uiSlice.ts
@@ -0,0 +1,7 @@
+type SideContainerViewType = "none" | "create_feed" | "create_provider" | "manage_feed" | "delete_feed"
+
+// Pass whatever params inside.
+interface SideContainerViewProp {
+    viewName: SideContainerViewType
+    params?: { id: string }
+}
