0c83961b413301238d602e7dd620c73739c1bce4
pchchv <ipchchv@gmail.com>
2024-09-19 09:48:13 +0300

    Implement an INITIAL_STATE

--- src/redux/feedSlice.ts
+++ src/redux/feedSlice.ts
@@ -1,7 +1,20 @@
 import { Feed, ReadingMode, SortMode } from "../types";
+import { v4 as uuidv4 } from 'uuid';
+import FEEDS from '../data/feeds';
 
 export interface FeedState {
     feed: Feed[],
     readingMode: ReadingMode
     sortMode: SortMode,
+}
+
+const INITIAL_STATE: FeedState = {
+    // collection of user created feeds
+    feed: [{
+        id: uuidv4(),
+        title: "My Feed",
+        feedProviders: [FEEDS[0]]
+    }],
+    readingMode: "card",
+    sortMode: "desc",
 }
