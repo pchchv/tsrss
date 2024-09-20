d647acb6cb0f70eb8832ee6f38d4f1c692292a0c
pchchv <ipchchv@gmail.com>
2024-09-20 10:59:41 +0300

    Implement parseXML func

--- src/utility/parseXML.ts
+++ src/utility/parseXML.ts
@@ -1,4 +1,5 @@
 import { Article } from "@/types";
+import {parseStringPromise} from "xml2js";
 
 function applyFilter(filter: string, articles: Article[]){
   if (filter === "today") {
@@ -36,4 +37,30 @@ function formatDate(pubDate: string){
   
   // else find how many days ag
   return daysDifference.toFixed() + "d"
+}
+
+export default async function parseXML(xml: any) {
+  let result: Article[] = await parseStringPromise(xml).then((result) => {
+    let items = result.rss.channel[0].item
+    // order them by newest
+    items.sort((a: any, b:any) => {
+      let dateA = new Date(a.pubDate[0])
+      let dateB = new Date(b.pubDate[0])
+      return dateB.getTime() - dateA.getTime()
+    });
+    
+    return items.map((item: any) => {
+      let image = item.image ? item.image[0] : undefined
+      let authors = ["Not Me"]
+      return {
+        authors: authors,
+        title: item.title[0],
+        description: item.description[0],
+        image: image,
+        link: item.link[0],
+        published: formatDate(item.pubDate[0]),
+      }
+    });
+  }).catch((err) => {});
+  return result
 }
