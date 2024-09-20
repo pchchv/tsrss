8e45c8394ad0a3bce8a66fa2124abd190478f398
pchchv <ipchchv@gmail.com>
2024-09-20 12:02:04 +0300

    Implement extractText

--- src/components/ArticleCard.tsx
+++ src/components/ArticleCard.tsx
@@ -1,5 +1,16 @@
 import { Article } from "@/types";
 
 interface ArticleCardProps{
-    article: Article
+  article: Article
+}
+
+const extractText = (html: string) => {
+  // this will still have HTML entities...
+  let plainText = html.replace(/<[^>]+>/g, '')
+  plainText = plainText.replace(/&nbsp;/g, ' ')
+  // if larger than 450 chars, truncate
+  if (plainText.length > 250) {
+    return plainText.slice(0, 250).trimEnd() + "..."
+  }
+  return plainText
 }
